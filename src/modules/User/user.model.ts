6787f7ff8107a5e37db2721334e5946caf41c0db
munir <smsirajulmonir@gmail.com>
2025-06-18 15:36:05 +0600

    user model, getapi ready

--- src/modules/User/user.model.ts
+++ src/modules/User/user.model.ts
@@ -1,33 +1,68 @@
-import { Schema } from "mongoose";
-import { IUser, UserModel } from "./user.interface";
-import { USER_ROLE } from "./user.constant";
+import { model, Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+import config from '../../app/config';
+import { IUser, UserModel } from './user.interface';
+import { USER_ROLE } from './user.constant';
 
 const userSchema = new Schema<IUser, UserModel>(
   {
-    name:{
-        type:String,
-        required:[true, 'User name is required']
+    name: {
+      type: String,
+      required: [true, 'User name is required'],
     },
-    email:{
-        type:String,
-        required:true,
-        match:[
+    email: {
+      type: String,
+      required: true,
+      match: [
         /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
         'Please provide a valid email address',
       ],
     },
-    password:{
-        type:String,
-        required:true,
-        select:false
+    password: {
+      type: String,
+      required: true,
+      select: false,
     },
-    role:{
-        type:String,
-        enum:[USER_ROLE.user, USER_ROLE.admin],
-        default:USER_ROLE.user
+    role: {
+      type: String,
+      enum: [USER_ROLE.user, USER_ROLE.admin],
+      default: USER_ROLE.user,
     },
   },
   {
-    timestamps:true
-  }  
+    timestamps: true,
+  },
 );
+
+// use hook to hash password before saving user
+userSchema.pre('save', async function (next) {
+  // eslint-disable-next-line @typescript-eslint/no-this-alias
+  const user = this;
+  // hashing password before saving
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+  next();
+});
+
+// use hook to empty password before sending response
+userSchema.post('save', function (doc, next) {
+  doc.password = '';
+  next();
+});
+
+// find user by using email
+userSchema.statics.isUserExistByEmail = async function (email: string) {
+  return await this.findOne({ email }).select('+password');
+};
+
+// Check if password is correct or not
+userSchema.statics.isPasswordMatched = async function (
+  plainTextPassword: string,
+  hashedPassword: string,
+) {
+  return await bcrypt.compare(plainTextPassword, hashedPassword);
+};
+
+export const User = model<IUser, UserModel>('User', userSchema);
--- src/modules/User/user.service.ts
+++ src/modules/User/user.service.ts
@@ -1,9 +1,24 @@
-import { JwtPayload } from "jsonwebtoken";
+import status from 'http-status';
+import { User } from './user.model';
+import AppError from '../../app/errors/AppError';
 
-const getSingleUserFromDB = async(userEmail:string,user:JwtPayload)=>{
-     
-}
+const getSingleUserFromDB = async (userEmail: string) => {
+  const result = await User.findOne({ email: userEmail });
 
-export const UserServices ={
-    getSingleUserFromDB
-}
+  if (!result) throw new AppError(status.NOT_FOUND, 'User not found');
+
+  return result;
+};
+
+const getAllUsersFromDB = async () => {
+ 
+    const result = await User.find();
+
+  return { result };
+};
+
+
+export const UserServices = {
+  getSingleUserFromDB,
+  getAllUsersFromDB
+};
